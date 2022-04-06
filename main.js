e18079effce3145eea136267cf83be6209a9a28d
LUKAS SANTOS VENANCIO <21276375@cfp127aluno.local>
2022-04-06 16:56:17 -0300

    modal completa.

--- main.js
+++ main.js
@@ -4,20 +4,7 @@ const criarCard = (anime) =>{
 
     const container = document.createElement('div')
     container.classList.add('card')
-    container.innerHTML = `<a href="#">
-        <img class="img-card" src="${anime.images.jpg.large_image_url}" title="${anime.title}">
-    </a>
-    <div class="modal-container" id="modal-container">
-        <div class="modal-content" id="modal-content>
-            <div class="first-column">
-                <img src="${anime.images.jpg.large_image_url}" alt="" title="${anime.title}">
-                <p>${anime.title}</p>
-            </div>
-            <div class="second-column">
-                <p>${anime.synopsis}</p>
-            </div>
-        </div>
-</div>`
+    container.innerHTML = `<img class="img-card" src="${anime.images.jpg.large_image_url}" title="${anime.title}" data-animeId="${anime.mal_id}">`
 
     return container                            
 
@@ -30,8 +17,6 @@ const pesquisarGenero = async (genre) =>{
     const response = await fetch(url)
     const data = await response.json()
 
-    // console.log(data)
-
     return data.data
 }
 
@@ -42,32 +27,16 @@ const carregarGenero = async(name, genre) =>{
     const title = document.getElementById('section-title')
     title.textContent = name
     const tipo = await pesquisarGenero(genre)
-    console.log(tipo)
+    // console.log(tipo)
 
     const cards = tipo.map(criarCard)
 
     containerCards.replaceChildren(...cards)
-    
-    // let genre = null
-
-    // if(name == 'Shounen'){
-    //     containerCards = document.getElementById('container-cards-shounen')
-    //     genre = 27
-    // }else if(name == 'Supernatural'){
-    //     containerCards = document.getElementById('container-cards-supernatural')
-    //     genre = 37
-    // }else if(name == 'Avant Garde'){
-    //     containerCards = document.getElementById('container-cards-avant-garde')
-    //     genre = 5
-    // }
 }
 
 const carregarShounen = () =>{carregarGenero('Shounen', 27)}
 const carregarSupernatural = () =>{carregarGenero('Supernatural', 37)}
 const carregarAvantGarde = () =>{carregarGenero('Avant Garde', 5)}
-// carregarGenero('Shounen', 27)
-// carregarGenero('Supernatural', 37)
-// carregarGenero('Avant Garde', 5)
 
 document.getElementById('supernatural').addEventListener('click', carregarSupernatural)
 document.getElementById('avant-garde').addEventListener('click', carregarAvantGarde)
@@ -81,6 +50,17 @@ const pesquisarAnime = async(anime) =>{
     const data = await response.json()
 
     console.log(data)
+
+    return data.data
+}
+
+const pesquisarAnimePorId = async(id) =>{
+    const url = `https://api.jikan.moe/v4/anime/${id}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    // console.log(data)
 
     return data.data
 }
@@ -100,17 +80,45 @@ const carregarAnime = async () =>{
 
 document.getElementById('pesquisar').addEventListener('click', carregarAnime)
 
-
-
 const abrirModal = () =>{
-    const container = document.getElementById('container-cards')
-    document.getElementById('modal-container').classList.add('active')
-
+    document.querySelector('.modal-container').classList.add('active')
 }
 
 const fecharModal = () =>{
-    document.getElementById('modal-container').classList.remove('active')
+    document.querySelector('.modal-container').classList.remove('active')
 }
 
-document.querySelector('.card').addEventListener('click', abrirModal)
-// document.getElementById('modal-content').addEventListener('click', fecharModal)
+const criarModal = (anime) =>{
+    const modal = document.createElement('div')
+    modal.classList.add('modal-content')
+    modal.innerHTML = `<div class="first-column">
+                            <img src="${anime.images.jpg.large_image_url}" alt="${anime.title}">
+                            <p>${anime.title}</p>
+                        </div>
+                        <div class="second-column">
+                            <p>${anime.synopsis}</p>
+                        </div>`
+
+    return modal                    
+}
+
+const carregarModal = async(id) =>{
+
+    const modalContent = document.getElementById('modal-container')
+
+    const data = await pesquisarAnimePorId(id)
+    const modal = criarModal(data) 
+
+    modalContent.replaceChildren(modal)
+}
+
+const handleClick = async( evento ) => {
+    const id = evento.target.dataset.animeid
+    carregarModal(id)
+
+
+    abrirModal()    
+}
+
+document.querySelector('.container-cards').addEventListener('click', handleClick)
+document.getElementById('modal-container').addEventListener('click', fecharModal)
